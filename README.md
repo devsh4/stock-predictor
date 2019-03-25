00b91985b4f8604cc622e065c3fcb2adee0e07e3
Dev <devsh2694@gmail.com>
2019-03-25 13:19:22 -0400

    final cleanup

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
-Stock prediction using ML technique (LSTM RNN)
-- Predicts daily, weekly/monthly price of BSE SENSEX 
+Stock prediction using LSTM RNN
+
+- Predicts daily, weekly/monthly price of BSE SENSEX
+
+
+Dataset
+
+- The dataset (.csv) comprises of past 20 year data of BSE SENSEX (primary index for India) from 1997-2017.
+- The dataset only has the closing price (which is used as the sole feature for training LSTM's)
+
+Model
+
+- The LSTM model has 2 hidden layers (50,200 cells respectively)
+- The model uses rmsprop and adam as training algorithms for separate tasks i.e. prior for weekly / longer term predictions and the latter for daily predictions
+- Linear activation (default) is used for the output layer
+- The learning rates and decay rates have been optimized accordingly
+
+Evaluation
+
+- In order to evaluate the model's daily prediction (1 time step ahead) we used RMSE and forecast Bias. Ideally both the values should be close to 0
+- In order to evaluate the model's weekly / longer term predictions (7 / 30 time steps ahead) we used directional accuracy as the performance measure.
+
+Run
+
+
+- In order the run the model, just fork / clone the repo and run the python file "stockpredictor.py" with the below command from the root folder:
+
+```
+python stockpredictor.py
+```
+
+- In order to debug or fine tune the model for testing or other purposes open the stockpredictor.py file in one of the many editors of python and play with model parameters like learning rate, dropout, cells, no. of hidden layers etc.
+
+
+Measures to avoid overfitting
+
+- Random shuffling of training windows to make sure the model doesn't fit to the sequences
+- Added dropout at hidden layers to ensure regularization
+- Use of early callback while training the model
---
+++ stockpredictor.py
@@ -0,0 +1,155 @@
+import lstm
+import daccuracy
+import time
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+from keras.wrappers.scikit_learn import KerasClassifier
+from keras.callbacks import EarlyStopping
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import mean_squared_error
+from math import sqrt
+
+def perf_stats(y_pred, y_test):
+
+    #For normalised values
+    forecast_errors = [y_test[i]-y_pred[i] for i in range(len(y_test))]
+
+    ##Mean forecast error or bias
+    mean_forecast_error = np.mean(forecast_errors)
+    print('Bias: %s' % mean_forecast_error)
+
+    ##RMSE
+    mse = mean_squared_error(y_test, y_pred)
+    rmse = sqrt(mse)
+    print('RMSE: %f' % rmse)
+
+    # Making the Confusion Matrix
+    #from sklearn.metrics import confusion_matrix
+    #cm = confusion_matrix(y_test, y_pred)
+
+    # For denormalised values
+    y_test = lstm.denormalise_windows(y_test, actual)
+    y_pred = lstm.denormalise_windows(y_pred, actual)
+
+    y_test = np.asarray(y_test)
+    y_pred = np.asarray(y_pred)
+
+    # try:
+    forecast_errors = [y_test[i] - y_pred[i] for i in range(len(y_test))]
+
+    ##Mean forecast error or bias
+    mean_forecast_error = np.mean(forecast_errors)
+    print('Bias: %s' % mean_forecast_error)
+
+    #except:
+        ##RMSE
+    mse = mean_squared_error(y_test, y_pred)
+    rmse = sqrt(mse)
+    print('RMSE: %f' % rmse)
+
+
+def plot_results(predicted_data, true_data):
+    tick_spacing = 0.05
+    fig = plt.figure(facecolor='white', figsize=(10,10))
+    fig.set_size_inches
+    ax = fig.add_subplot(111)
+    ax.yaxis.set_major_locator(ticker.MultipleLocator(tick_spacing))
+    ax.plot(true_data, label='True Data', color = 'blue')
+    plt.plot(predicted_data, label='Prediction', color = 'green')
+    plt.legend()
+    plt.show()
+
+
+def plot_results_multiple(predicted_data, true_data, prediction_len):
+    fig = plt.figure(facecolor='white', figsize=(10,10))
+    ax = fig.add_subplot(111)
+    ax.plot(true_data, label='True Data', color = 'blue')
+    for i, data in enumerate(predicted_data):
+        padding = [None for p in range(i * prediction_len)]
+        plt.plot(padding + data, label='Prediction', color = 'magenta')
+
+    plt.xlim(0,150)
+    plt.show()
+
+
+def grid_search():
+
+    # Applying Grid Search to find the best model and the best parameters
+    batch_size = [50, 100, 200, 500]
+    #epochs =  [1, 5, 10]
+    parameters = dict(batch_size=batch_size)
+    m = KerasClassifier(build_fn = lstm.build_model, layers = [1, window_size, 200, 1])
+    grid_search = GridSearchCV(estimator = m,
+                           param_grid = parameters,
+                           scoring = 'neg_mean_squared_error',
+                           n_jobs = -1)
+
+    grid_result = grid_search.fit(X_train, y_train)
+    best_parameters = grid_result.best_params_
+    print("Best params - ", best_parameters)
+
+
+# Main Function
+if __name__=='__main__':
+    start_time = time.time()
+    epochs  = 50
+    window_size = 50
+    prediction_length = 5
+
+    seed = 7
+    np.random.seed(seed)
+
+    # higher the number, higher the space required
+    batch = 256
+
+    # Training and testing data
+    X_train, y_train, X_test, y_test, actual = lstm.get_data('bse_train.csv', window_size, True)
+
+    # for TECHM stock data
+    # X_train, y_train, X_test, y_test, actual = lstm.get_data('techm.csv', window_size, True)
+    print('>>> Data processed...')
+
+    #for daily predictions
+    model = lstm.build_model([1, window_size, 200, 1])
+
+    # if val loss isnt decreasing, early callback to stop training
+    early_stopping = EarlyStopping(monitor='val_loss', patience = 3)
+
+    # Fit model with batch size and epochs
+    history = model.fit(X_train, y_train, batch_size=batch, nb_epoch=epochs, validation_split=0.1, callbacks=[early_stopping])
+
+    # Metrics which are stored for each epoch
+    # print(history.history.keys())
+
+    #summarize history
+    fig = plt.figure(facecolor='white', figsize=(8,8))
+    ax = fig.add_subplot(111)
+    ax.plot(history.history['loss'], color= 'blue')
+    plt.plot(history.history['val_loss'], color = 'red')
+    plt.title('model train vs validation loss')
+    plt.ylabel('loss')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'validation'], loc='upper right')
+    plt.show()
+
+    # Call predict function with window size and prediction length
+    predictions = lstm.predict_sequences_multiple(model, X_test, window_size, prediction_length)
+
+    # Call point by point predict method
+    y_pred = lstm.predict_point_by_point(model, X_test)
+
+    print('Training time : ', time.time() - start_time)
+
+    # Plot multiple preds
+    plot_results_multiple(predictions, y_test, prediction_length)
+    #************************
+
+    # Plot daily preds
+    plot_results(y_pred, y_test)
+
+    # Get Performance Stats
+    perf_stats(y_pred, y_test)
+
+    # Get directional accuracy of n days ahead prediction
+    daccuracy.calc_directional_accuracy(y_test, predictions, prediction_length)
